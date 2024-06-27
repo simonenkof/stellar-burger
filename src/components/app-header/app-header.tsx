eb380fe3fa5387fa32230ccad94b1a2016e40dfb
simonenkof <simonenko.f.a@gmail.com>
2024-06-28 00:14:38 +0300

    register and login user

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,10 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../../src/services/store';
+import { selectCurrentUser } from '../../../src/services/slices/userSlice';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const userData = useSelector(selectCurrentUser);
+
+  return <AppHeaderUI userName={userData.name} />;
+};
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,23 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { loginUserApi } from '@api';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from '../../../src/services/store';
+import { setUser } from '../../../src/services/slices/userSlice';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigation = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    loginUserApi({ email: email, password: password }).then((data) => {
+      dispatch(setUser({ name: data.user.name, email: data.user.email }));
+      navigation('/');
+    });
   };
 
   return (
