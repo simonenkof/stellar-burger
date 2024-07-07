a5a18f3ab67e7e4cbb1f45eadb8cc6fbe76b3a06
simonenkof <simonenko.f.a@gmail.com>
2024-07-07 12:28:34 +0300

    redirects when an unauthorized user attempts to place an order

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -2,16 +2,22 @@ import { FC, useMemo } from 'react';
 import { BurgerConstructorUI } from '@ui';
 import { useSelector } from '../../services/store';
 import { selectConstructorItems } from '../../services/slices/consturctorBurgerSlice';
+import { selectCurrentUser } from '../../../src/services/slices/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const BurgerConstructor: FC = () => {
   /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
   const constructorItems = useSelector(selectConstructorItems);
+  const user = useSelector(selectCurrentUser);
+  const navigate = useNavigate();
 
   const orderRequest = false;
 
   const orderModalData = null;
 
   const onOrderClick = () => {
+    if (!user.isAuth) navigate('/login');
+
     if (!constructorItems.bun || orderRequest) return;
   };
   const closeOrderModal = () => {};
