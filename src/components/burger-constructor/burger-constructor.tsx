a1e8c53bfad5483b30b29851f7ba6525cb8cf169
simonenkof <simonenko.f.a@gmail.com>
2024-07-07 18:39:50 +0300

    create order

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,26 +1,38 @@
 import { FC, useMemo } from 'react';
 import { BurgerConstructorUI } from '@ui';
-import { useSelector } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
 import { selectConstructorItems } from '../../services/slices/consturctorBurgerSlice';
 import { selectCurrentUser } from '../../../src/services/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { orderBurgerApi } from '@api';
+import { setOrderData, setRequested, selectOrderData, selectRequested } from '../../../src/services/slices/orderSlice';
 
 export const BurgerConstructor: FC = () => {
-  /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const constructorItems = useSelector(selectConstructorItems);
   const user = useSelector(selectCurrentUser);
-  const navigate = useNavigate();
-
-  const orderRequest = false;
-
-  const orderModalData = null;
+  const orderRequest = useSelector(selectRequested);
+  const orderModalData = useSelector(selectOrderData);
 
   const onOrderClick = () => {
     if (!user.isAuth) navigate('/login');
 
     if (!constructorItems.bun || orderRequest) return;
+
+    const ingredients: string[] = [];
+    constructorItems.ingredients.forEach((ingredient) => ingredients.push(ingredient._id));
+
+    dispatch(setRequested(true));
+
+    orderBurgerApi(ingredients).then((res) => {
+      dispatch(setOrderData(res.order));
+      dispatch(setRequested(false));
+    });
   };
-  const closeOrderModal = () => {};
+  const closeOrderModal = () => {
+    dispatch(setOrderData(null));
+  };
 
   const price = useMemo(
     () =>
