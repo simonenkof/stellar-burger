f8870bb94406dea92ed17b8a89045fdbc283c65c
simonenkof <simonenko.f.a@gmail.com>
2024-08-07 13:27:56 +0300

    fix protected-route component

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -6,15 +6,14 @@ import { selectCurrentUser } from '../../../src/services/slices/userSlice';
 export const ProtectedRoute = ({ children, onlyUnAuth }: ProtectedRouteProps) => {
   const user = useSelector(selectCurrentUser);
   const location = useLocation();
+  const from = location.state?.from || '/';
 
   if (onlyUnAuth && user.isAuth) {
-    const back = location.state?.from || { pathname: '/' };
-
-    return <Navigate to={back} replace />;
+    return <Navigate to={from} />;
   }
 
   if (!onlyUnAuth && !user.isAuth) {
-    return <Navigate to='/login' replace />;
+    return <Navigate to='/login' state={{ from: location }} />;
   }
 
   return children;
