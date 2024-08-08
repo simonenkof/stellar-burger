3b3bc46fca5cb407c37fa89158b33b7898136293
simonenkof <simonenko.f.a@gmail.com>
2024-08-08 15:59:15 +0300

    update modal routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -86,6 +86,7 @@ const App = () => {
             </ProtectedRoute>
           }
         />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
       </Routes>
 
       {background && (
